71b15beb491fa53250d095a33b6cb351699e50c7
RanyunDai <60996040+RanyunDai@users.noreply.github.com>
2021-10-05 19:12:46 -0400

    front end

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Juice-Quiz
+Starter code for quiz-app beginner react project
+
+- clone repo
+- npm install
+- npm start
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+	const handleNextQuestion=(isCorrect)=>{
+		const nextQuestion=currentQuestion+1;
+		if(nextQuestion<questions.length){
+			if(isCorrect){
+			 	setScore(currentScore+1);
+			}else{
+
+			}
+			setCurrentQuestion(nextQuestion);
+		}else{
+			setShowScore(true);
+		}
+	}
+	
+	const [currentQuestion,setCurrentQuestion]=useState(0);
+	const [showScore, setShowScore]=useState(false);
+	const [currentScore,setScore]=useState(0);
+	return (
+		<div className='app'>
+			{/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+			{showScore ? (
+				<div className='score-section'>You scored  out of {currentScore}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question 1</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						 {questions[currentQuestion].answerOptions.map((answerOptions)=>
+						 <button onClick={()=>handleNextQuestion(answerOptions.isCorrect)}>{answerOptions.answerText}</button>
+						 )}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
