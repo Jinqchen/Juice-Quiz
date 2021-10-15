341d462181b26e94c69979bc90f723c848927fc0
Jinqchen <jinquan.chen@stonybrook.edu>
2021-10-14 22:31:32 -0400

    root commit

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req,res)=>{
-console.log('requesr made');
-});
-
-server.listen(3000,'localhost',()=>{
-    console.log('listening for requests on port 3000')
-})
