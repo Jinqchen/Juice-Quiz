cc9a441a4e0223e2cd1b8af2f796777a0d93d658
Jinqchen <jinquan.chen@stonybrook.edu>
2021-10-22 22:59:39 -0400

    initial

---
+++ client/build/precache-manifest.dde789b9fd9bddf8608272a4c3bb131b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebead0594ea24483bdd0b29d4d2a348a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6646141b3aeb767d35f6",
+    "url": "/static/css/main.ccdb66c2.chunk.css"
+  },
+  {
+    "revision": "06a1545ef7462adf9eab",
+    "url": "/static/js/2.20a6a03f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.20a6a03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6646141b3aeb767d35f6",
+    "url": "/static/js/main.20250606.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,98 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port =3001;
+var mysql = require("mysql");
+
+
+
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("/", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+var con = mysql.createConnection({
+	host: "us-cdbr-east-04.cleardb.com",
+	user: "b0fb64176d0a67",
+	password: "1322f121",
+	database: "heroku_a8e492587e5e18a",
+	port:3306,
+});
+
+
+
+
+app.get ('/api/get',(req,res)=>{
+   
+    console.log("Connected!");
+    con.query(
+		"select count(QuestionID) from quizquestion where QID=1 ;", 
+		function (err1, result) {
+		if (err1) throw err1;
+	    file={};
+		file["count(QuestionID)"]=result[0]["count(QuestionID)"];
+		for(let i = 0; i < result[0]["count(QuestionID)"]; i++){
+			con.query("select Qtext from quizquestion where QID=1 AND QuestionID="+String(i+1)+";", 
+			function (err2, result2) {
+			 if (err2) throw err2;
+			 file[i+1]= {};
+			 var str = JSON.stringify(result2[0]);
+             var obj = JSON.parse(str)
+			 file[i+1]['Qtext']=obj['Qtext'];
+			 });
+			
+			con.query("select Optionx ,correctness  from quizoptions where QID=1 AND QuestionID="+String(i+1)+";", 
+			function (err2, result2) {
+			if (err2) throw err2;
+		    var str = JSON.stringify(result2);
+            var obj = JSON.parse(str);
+            file[i+1]['Optionx']=obj;
+			});
+	   }
+       }
+	  )
+	  res.send(file);
+	}
+  );
+ 
+
+
+// app.post("/api/register", (req, res) => {
+// 	const username = req.body.username;
+// 	const password = req.body.password;
+//     const email  = req.body.email;
+// 	const id = 1;
+// 	bcrypt.hash(password, saltRounds, (err, hash) => {
+// 	  if (err) {
+// 		console.log(err);
+// 	  }
+  
+// 	  con.query(
+// 		"INSERT INTO users (UID,Uname,Uemail,Upass) VALUES (?,?,?,?)",
+// 		[id,username,email, hash],
+// 		(err, result) => {
+// 		  console.log(err);
+// 		}
+// 	  );
+// 	});
+//   }); 
+
+
+	
+ 
+app.listen(process.env.PORT || 3001,()=>{
+  console.log('listening for requests on port'+ process.env.PORT);
+})
+
+
+
